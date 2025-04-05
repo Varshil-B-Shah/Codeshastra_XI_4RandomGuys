0408c34b316e5457b118f6fef1a794e709499075
Varshil <varshil4247shah@gmail.com>
2025-04-05 11:54:04 +0530

    yello

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import ImageUploader from "@/components/ImageUploader";
+import IndiaMap from "@/components/IndiaMap";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full h-full overflow-x-hidden">
-      <ImageUploader />
+      {/* <ImageUploader /> */}
+      <IndiaMap />
     </div>
   );
 }
